faae1cacef07c375086883a131d187ea6a20651b
omkar pawar <omkarpawarfull@gmail.com>
2017-07-06 01:32:49 -0500

    Handled exceptions
    Added error messages

--- CirrusSI/src/Palindrome/PalindromeClass.java
+++ CirrusSI/src/Palindrome/PalindromeClass.java
@@ -1,22 +1,19 @@
 package Palindrome;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.*;
 
 public class PalindromeClass extends Application{
 
 	Button find,clear;
 	TextField lower,higher,answer;
-	Label label1,label2,label3;
+	Label label1,label2,label3,errorMessage;
 	public static void main(String[] args) {
 		launch(args);
 	}
@@ -26,25 +23,20 @@ public class PalindromeClass extends Application{
 		primaryStage.setTitle("Finding Higest Palindrome");
 		find =new Button("Find Palindrome");
 		clear=new Button("Clear");
-		lower=new TextField("Enter Lower Bound Number");
-		higher=new TextField("Enter Higher Bound Number");
+		lower=new TextField("Lower Bound");
+		higher=new TextField("Higher Bound");
 		answer=new TextField();
 		label1=new Label("Lower");
 		label2=new Label("Higher");
 		label3=new Label("Answer");
-		/*find.setOnAction(new EventHandler<ActionEvent>(){
-			@Override
-			public void handle(ActionEvent event) {
-				
-					System.out.println("Find clicked");
-				
-			}
-		});*/
+		errorMessage=new Label();
 		find.setOnAction(e -> {
+			answer.setText("");
+			errorMessage.setText("");
 			long l=Long.parseLong(lower.getText());                    // enter number between -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
 			long h=Long.parseLong(higher.getText());
 			if(l==h || l>h){
-				System.out.println("Enter a valid range");
+				errorMessage.setText("Enter a valid range");
 			}
 			String reverse="";
 			String toCompare="";
@@ -59,25 +51,37 @@ public class PalindromeClass extends Application{
 				}
 				h--;
 				System.out.println(h);
-				if(flag==0){
-					answer.setText("No Palindrom in the Range");
-				}
+				
+			}
+			if(flag==0){
+				errorMessage.setText("No Palindrom in the Range");
 			}
 		});
-		
+		lower.setOnMouseClicked(e -> {
+			lower.setText("");
+		});
+		higher.setOnMouseClicked(e -> {
+			higher.setText("");
+		});
 		clear.setOnAction(e -> {
 			lower.setText("");
 			higher.setText("");
 			answer.setText("");
+			errorMessage.setText("");
 		});
 		HBox h1=new HBox(10);
+		h1.setPadding(new Insets(0, 10, 5, 0)); 
 		h1.getChildren().addAll(label1,lower);
 		HBox h2=new HBox(10);
 		h2.getChildren().addAll(label2,higher);
+		h2.setPadding(new Insets(0, 10, 5, 0));
 		HBox h3=new HBox(10);
 		h3.getChildren().addAll(label3,answer);
+		HBox h4=new HBox(10);
+		h4.getChildren().addAll(clear,find);
 		VBox layout=new VBox(10);
-		layout.getChildren().addAll(h1,h2,clear,find,h3);
+		layout.setPadding(new Insets(0, 20, 10, 20));
+		layout.getChildren().addAll(h1,h2,h4,h3,errorMessage);
 		
 		Scene scene=new Scene(layout,300,250);
 		primaryStage.setScene(scene);
