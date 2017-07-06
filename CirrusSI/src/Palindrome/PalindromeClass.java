f1b45c9e0d6b3eb75c817935ecda6a7806685c8e
omkar pawar <omkarpawarfull@gmail.com>
2017-07-05 22:49:31 -0500

    using lamda functions
    Added new buttons, textfield, labels.
    Basic GUI done

--- CirrusSI/src/Palindrome/PalindromeClass.java
+++ CirrusSI/src/Palindrome/PalindromeClass.java
@@ -4,14 +4,19 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.*;
 
 public class PalindromeClass extends Application{
 
-	
-
-	Button find;
+	Button find,clear;
+	TextField lower,higher;
+	Label label1,label2;
 	public static void main(String[] args) {
 		launch(args);
 	}
@@ -19,18 +24,33 @@ public class PalindromeClass extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("Finding Higest Palindrome");
-		find =new Button();
-		find.setText("Find Palindrome");
-		find.setOnAction(new EventHandler<ActionEvent>(){
+		find =new Button("Find Palindrome");
+		clear=new Button("Clear");
+		lower=new TextField("Enter Lower Bound Number");
+		higher=new TextField("Enter Higher Bound Number");
+		label1=new Label("Lower");
+		label2=new Label("Higher");
+		/*find.setOnAction(new EventHandler<ActionEvent>(){
 			@Override
 			public void handle(ActionEvent event) {
-				if(event.getSource()==find){
+				
 					System.out.println("Find clicked");
-				}
+				
 			}
+		});*/
+		find.setOnAction(e -> {
+			System.out.println("It works");
 		});
-		StackPane layout=new StackPane();
-		layout.getChildren().add(find);
+		clear.setOnAction(e -> {
+			lower.setText("");
+			higher.setText("");
+		});
+		HBox h1=new HBox(20);
+		h1.getChildren().addAll(label1,lower);
+		HBox h2=new HBox(20);
+		h2.getChildren().addAll(label2,higher);
+		VBox layout=new VBox(20);
+		layout.getChildren().addAll(h1,h2,clear,find);
 		
 		Scene scene=new Scene(layout,300,250);
 		primaryStage.setScene(scene);
